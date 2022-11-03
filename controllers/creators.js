3b84ceb322f0289d518b16c1023b05941eb70483
ruraliz <ruraliz12@gmail.com>
2022-11-02 21:24:53 -0700

    add: seed-creators

--- controllers/creators.js
+++ controllers/creators.js
@@ -5,30 +5,39 @@ const Creator = require('../models/creator');
 const passport = require('passport');
 const axios = require("axios")
 const token=process.env.TOKEN ;
-const clientKey = process.env.CLIENT_KEY;
-const clientSecret = process.env.CLIENT_SECRET;
+// const clientKey = process.env.CLIENT_KEY;
+// const clientSecret = process.env.CLIENT_SECRET;
 
- axios({
-    url: 'https://api.podchaser.com/graphql',
-    method: 'post',
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    data: {
-      query: `
-      {
-        creator(identifier: CreatorIdenitfier) {
-            name,
-            formalName,
-            apcid,
-            bio
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const searchTerm= req.body.q
+    const options= {
+        method: 'post',
+        url: "https://api.podchaser.com/graphql",
+        params: {q: searchTerm},
+        headers: {
+            Authorization: `Bearer ${token}`
         }
+    };
+ axios.get( "https://api.podchaser.com/graphql", options).then(function(response){
+    // url: 'https://api.podchaser.com/graphql',
+    // method: 'post',
+    // headers: {
+    //   Authorization: `Bearer ${token}`
+    // },
+    // data: {
+    console.log(response.data.creators);
+    if (response.status === 200 && response.data.creators && response.data.creators.length) {
+        res.status(200).render('/', {creators: response.data.creators});
+    } else {
+        res.status(404).render('404');
     }
-        `
-    }
-  }).then(result => {
-    console.log(result.data);
-  }).catch(err => {
-    console.log(err)
-    });
+  }).catch(function (error) {
+      console.error(error);
+  });
+  });
+
+
+
+     
 module.exports = router;
---
+++ seed/seed-creators.js
@@ -0,0 +1,92 @@
+"use strict";
+require("dotenv").config();
+const mongoose = require("mongoose");
+// connect to datbase
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true });
+const db = mongoose.connection;
+const clientKey = process.env.CLIENT_KEY;
+const clientSecret = process.env.CLIENT_SECRET;
+let token;
+db.once("open", () => {
+  console.log(`Connected to MongoDB at HOST: ${db.host} and PORT: ${db.port}`);
+});
+
+db.on("error", (error) => {
+  console.log(`Database Error: ${error}`);
+});
+
+const axios = require("axios");
+const Creator = require("../models/creator");
+const { insertMany } = require("../models/creator");
+
+axios({
+  url: "https://api.podchaser.com/graphql",
+  method: "post",
+  data: {
+    query: `
+      mutation {
+          requestAccessToken(
+              input: {
+                  grant_type: CLIENT_CREDENTIALS
+                  client_id: "${clientKey}"
+                  client_secret: "${clientSecret}"
+              }
+          ) {
+              access_token
+          }
+      }
+        `,
+  },
+})
+  .then((result) => {
+    console.log(result.data);
+    token = result.data.data.requestAccessToken.access_token;
+    const randCreators = {
+      method: "post",
+      url: "https://api.podchaser.com/graphql",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        query: ` query {
+          creators {
+              data {
+                  name,
+                  bio,
+                  location
+              }
+          }  
+        }    
+              `,
+      },
+    };
+    axios
+      .request(randCreators)
+      .then((response) => {
+        console.log(response);
+        let newCreators = [];
+        response.data.data.creators.data.forEach((creator) => {
+          newCreators.push({
+            name: creator.name,
+            bio: creator.bio,
+            location: creator.location,
+          });
+        });
+        Creator.insertMany(newCreators)
+        .then(response => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log('error', error)
+        })
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
+
+
