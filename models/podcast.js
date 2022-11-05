f8d73d4e9b9bf0264184524b7f2dc0bd1e15cc9b
hninmabalo <hninmabalo@gmail.com>
2022-11-05 11:54:03 -0700

    add: podcast associatin with comment

--- models/podcast.js
+++ models/podcast.js
@@ -4,7 +4,11 @@ const { Schema } = mongoose;
 const podcastSchema = new Schema({
     imageUrl: String,
     title: String,
-    description: String
+    description: String,
+    comments: {
+        type: mongoose.Types.ObjectId,
+        ref: "Comment"
+    }
 });
 
 const Podcast = mongoose.model('Podcast', podcastSchema);
