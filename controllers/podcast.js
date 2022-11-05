5f81121d7e514b8d9726524b069642459bde53fb
Hnin.M <107435213+hninmabalo@users.noreply.github.com>
2022-11-05 11:59:10 -0700

    podcast association with comment  (#10)
    * add: podcast controllers and seeding podcast data
    
    * fix: podcast seed file
    
    * add: podcasts routes in controllers
    
    * add: podcast associatin with comment

--- controllers/podcast.js
+++ controllers/podcast.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const Podcast = require('../models/podcast');
+const Comment = require('../models//comment');
 const passport = require('passport');
 const axios = require("axios")
 let token;
@@ -149,5 +150,50 @@ router.delete('/:title', (req, res) => {
     res.json({ message : "Error ocurred"});
   })
 });
+
+
+//=================== association with comment =======================
+
+// GET a podcast's comments
+router.get('/:title/comments', (req, res) => {
+  Podcast.findOne(req.params.title).populate('comments').exec()
+  .then(podcast => {
+      console.log('This is the podcast', podcast);
+  })
+})
+
+// create a comment on a podcast
+router.post('/:title/comments', (req, res) => {
+  Podcast.findOne(req.params.title)
+  .then(podcast => {
+      console.log('Heyyy, this is the podcast', podcast);
+      // create and pust comment inside of podcast
+      Comment.create({
+        pcid: req.body.pcid,
+        user: req.body.user,
+        rating: req.body.rating,
+        content: req.body.content,
+        url: req.body.url,
+        reviewedAt: req.body.reviewAt,
+        modifiedDate: req.body.modifiedDate,
+        reply: req.body.reply,
+      })
+      .then(comment => {
+          podcast.comments.push(comment);
+          // save the podcast
+          podcast.save();
+          res.redirect(`/podcasts/${req.params.title}`);
+      })
+      .catch(error => { 
+          console.log('error', error);
+          res.json({ message: "Error ocurred" });
+      });
+  })
+  .catch(error => { 
+      console.log('error', error);
+      res.json({ message: "Error ocurred" });
+  });
+});
+
 
 module.exports = router;
